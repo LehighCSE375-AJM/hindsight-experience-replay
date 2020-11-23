50e21d18041c92ce60f15d625ca846856324a2d7
Jacob Oakman <jacoboakman@gmail.com>
2020-11-23 14:45:35 -0500

    Changed to use list of structs to resemble dicts

--- cpp/adam.h
+++ cpp/adam.h
@@ -9,26 +9,40 @@
  */
 class Adam {
 private:
-		list<Matrix*> param_groups;
-		double lr = 0.01;
-		double betas[2] = {0.9, 0.999};
-		double eps = 0.00000008;
-		double weight_decay = 0;
-		bool amsgrad = false;
+		struct Adam_Group {
+			Matrix* params;
+			double lr = 1E-3L;
+			double betas[2] = {0.9L, 0.999L};
+			double eps = 1E-8L;
+			double weight_decay = 0;
+			bool amsgrad = false;
+		};
+		list<Adam_Group*> param_groups;
 public:
 		// @param params should be an iterable of tensors of dicts
 		// @param lr is the learning rate, default 0.01
-		Adam(list<Matrix*> params, double lr) {
-			this->lr = lr;
+		Adam(list<Matrix*> params, double lr = 1E-3L) {
+			Adam_Group* group = new Adam_Group();
+			group->lr = lr;
 
 			for (auto it = params.begin(); it != params.end(); ++it) {
-				param_groups.push_back(*it);
+				Adam_Group* group = new Adam_Group();
+				group->lr = lr;
+				group->params = *it;
+				param_groups.push_back(group);
 			}
 		}
 
-		void zero_grad() {
+		void zero_grad(bool set_to_none = false) {
 			for (auto it = this->param_groups.begin(); it != this->param_groups.end(); ++it) {
-				
+				for () {
+					if (set_to_none) {
+
+					}
+					else {
+						  
+					}
+				}
 			}
 		}
 
