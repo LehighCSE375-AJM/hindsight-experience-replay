e0cf7698f2fc6e5e04ff57b081da48a8634b0462
Jacob Oakman <jacoboakman@gmail.com>
2020-11-23 14:09:18 -0500

    Started work implementing an adam optimizer

---
+++ cpp/adam.h
@@ -0,0 +1,32 @@
+#include "matrix.h"
+#include <list>
+
+class Adam {
+private:
+		list<Matrix*> param_groups;
+		double lr = 0.01;
+		double betas[2] = {0.9, 0.999};
+		double eps = 0.00000008;
+		double weight_decay = 0;
+		bool amsgrad = false;
+public:
+		// @param params should be an iterable of tensors of dicts
+		// @param lr is the learning rate, default 0.01
+		Adam(list<Matrix*> params, double lr) {
+			this->lr = lr;
+
+			for (auto it = params.begin(); it != params.end(); ++it) {
+				param_groups.push_back(*it);
+			}
+		}
+
+		void zero_grad() {
+			for (auto it = this->param_groups.begin(); it != this->param_groups.end(); ++it) {
+
+			}
+		}
+
+		void step() {
+
+		}
+};
